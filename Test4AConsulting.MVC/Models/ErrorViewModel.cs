32ba342d5c7774d6a3e280d2de3f85295cef12da
iqc <77848472+Issatonk@users.noreply.github.com>
2026-08-19 16:22:52 +0300

    chore: remove duplicate project outside src

--- Test4AConsulting.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Test4AConsulting.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
